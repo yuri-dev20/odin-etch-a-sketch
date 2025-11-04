bec2496e7039e050523f63fb15df7bcc2e64ea25
yuri-dev20 <yuriglauco945@gmail.com>
2025-11-04 17:03:04 -0300

    fixed board size, finished game logic,implemented reset button and added some style to the page

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const board = document.querySelector("#board");
 const boardSize = document.querySelector("#boardSize")
-const btn = document.querySelector("#btn");
+const submit = document.querySelector(".submit");
+const reset = document.querySelector(".reset");
 
 const createGrid = () => {
     if (boardSize.value) {
@@ -9,7 +10,6 @@ const createGrid = () => {
         for(let i = 0; i < value; i++) {
             createDivRow = document.createElement("div")
             createDivRow.classList.add("gridRow");
-            // createDivRow.textContent = "a";
             board.appendChild(createDivRow);
     
         }
@@ -17,7 +17,6 @@ const createGrid = () => {
         for(let i = 0; i < 16; i++) {
             createDivRow = document.createElement("div")
             createDivRow.classList.add("gridRow");
-            // createDivRow.textContent = "a";
             board.appendChild(createDivRow);
     
         }
@@ -34,7 +33,7 @@ const createGridItems = () => {
         gridRows.forEach(row => {
             for (let i = 0; i < value; i++) {
                 const rowItem = document.createElement("div");
-                rowItem.textContent = "b";
+                rowItem.classList.add("paint");
                 row.appendChild(rowItem);
     
             }
@@ -47,7 +46,7 @@ const createGridItems = () => {
         gridRows.forEach(row => {
             for (let i = 0; i < 16; i++) {
                 const rowItem = document.createElement("div");
-                rowItem.textContent = "b";
+                rowItem.classList.add("paint");
                 row.appendChild(rowItem);
     
             }
@@ -59,11 +58,24 @@ const createGridItems = () => {
 
 const drawBoard = () => {
     board.innerHTML = "";
-    board.style.border = "8px solid rgb(234, 218, 77)"
     createGrid();
     createGridItems();
+    
+    const squares = document.querySelectorAll(".paint");
+    squares.forEach(square => {
+        square.addEventListener("mouseover", () => {
+            square.style.background = "black";
+
+        });
+
+    });
 }
 
-document.addEventListener("DOMContentLoaded", drawBoard)
-
-btn.addEventListener("click", drawBoard)
+document.addEventListener("DOMContentLoaded", drawBoard);
+submit.addEventListener("click", drawBoard);
+reset.addEventListener("click", () => {
+    board.innerHTML = "";
+    createGrid();
+    createGridItems();
+    
+});
